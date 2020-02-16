5b68f7c5eab9cd605a536a99a78d04aa1c3f1056
Bogdan Voskanov <bogdanvoskanov@gmail.com>
2020-02-16 12:09:28 +0300

    Animations created

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,31 @@
 $(function(){
+	$(window).scroll(function () {
+
+		$('.benefits-title').each(function () {
+			var elPos = $(this).offset().top;
+			var topOfWindow = $(window).scrollTop();
+			if (elPos < topOfWindow + 500) {
+				$(this).addClass('fadeInLeft');
+			}
+		})
+
+		$('.benefits-item').each(function () {
+			var elPos = $(this).offset().top;
+			var topOfWindow = $(window).scrollTop();
+			if (elPos < topOfWindow + 500) {
+				$(this).addClass('fadeInLeft');
+			}
+		})
+
+		$('section').each(function () {
+			var elPos = $(this).offset().top;
+			var topOfWindow = $(window).scrollTop();
+			if (elPos < topOfWindow + 500) {
+				$(this).addClass('fadeInLeft');
+			}
+		})
+
+
+	})
 
 });
