fda7616fc28d046da525ad25c3d71f0320a56801
Bogdan Voskanov <bogdanvoskanov@gmail.com>
2020-03-01 22:53:30 +0300

    Modal windows created

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,6 @@
-$(function(){
+'use strict';
+
+$(function () {
 	$(window).scroll(function () {
 
 		$('.benefits-title').each(function () {
@@ -75,7 +77,55 @@ $(function(){
 		})
 
 
-
 	})
 
+
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+// Get the <span> element that closes the modal
+var span = document.getElementById('close');
+// Get the modal response
+var modalResponse = document.getElementById('myModalResponse');
+// Get the button that opens modal response
+var closeFormBtn = document.getElementById('closeFormBtn');
+// Get the button that closes the modal Response
+var closeResponse = document.getElementById('closeResponse');
+
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+	modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = 'none';
+}
+
+window.onclick = function (event) {
+	if (event.target === modal) {
+		modal.style.display = 'none';
+	}
+}
+
+closeResponse.onclick = function () {
+	modalResponse.style.display = 'none';
+}
+
+window.onclick = function (event) {
+	if (event.target === modalResponse) {
+		modalResponse.style.display = 'none';
+	}
+}
+
+// When the user clicks on button in first modal, open modal response
+closeFormBtn.onclick = function () {
+	modal.style.display = 'none';
+	modalResponse.style.display = 'block';
+}
+
